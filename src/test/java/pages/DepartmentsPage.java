35995949681acc33bdf2a0b3f26faf0d66a0f979
özcan <ozxcan91@gmail.com>
2024-03-18 13:46:38 +0100

    Department 1

--- src/test/java/pages/DepartmentsPage.java
+++ src/test/java/pages/DepartmentsPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.ParallelDriver;
 
@@ -8,5 +10,18 @@ public class DepartmentsPage {
     public DepartmentsPage() { // bu constructor her page sayfasında standart olacak
         PageFactory.initElements(ParallelDriver.getDriver(), this);
     }
-String s;
+@FindBy(xpath = "(//button[@type='button'])[3]")
+    public WebElement add_new_Department;
+
+    @FindBy(xpath = "//b[.='Finance Department']")
+    public WebElement Finance_Department;
+     @FindBy(xpath = "//b[.='Logistics Department']")
+    public WebElement Logistics_Department;
+      @FindBy(xpath = "//b[.='Marketing and Sales Department']")
+    public WebElement Marketing_and_Sales_Department;
+@FindBy(xpath = "//b[.='Quality Assurance Department']")
+    public WebElement Quality_Assurance_Department;
+@FindBy(xpath = "//b[.='personel']")
+    public WebElement personel_Department;
+
 }
---
+++ src/test/resources/features/Departments.feature
@@ -0,0 +1,3 @@
+@Department
+  Feature:
+    Scenario:
