d5a4c6ec0203e810dc48cda4866e986ec0e17f0f
özcan <ozxcan91@gmail.com>
2024-03-18 14:49:27 +0100

    Department 2

---
+++ src/test/java/stepdefinitions/DepartmentsSD.java
@@ -0,0 +1,15 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.*;
+
+public class DepartmentsSD {
+    @Given("Der Benutzer klickt auf der linken Seite der Seite auf „Abteilung“.")
+    public void der_benutzer_klickt_auf_der_linken_seite_der_seite_auf_abteilung() {
+
+    }
+    @Then("Der Benutzer uberpruft, ob die Finanzabteilung angezeigt wird")
+    public void der_benutzer_uberpruft_ob_die_finanzabteilung_angezeigt_wird() {
+
+    }
+
+}
--- src/test/resources/features/Departments.feature
+++ src/test/resources/features/Departments.feature
@@ -1,3 +1,10 @@
 @Department
-  Feature:
-    Scenario:
+  Feature:US_0007
+    Background:
+      Given Benutzer geht zur URL
+      And Benutzer meldet sich an
+      Scenario: TC_0007_001
+        Given Der Benutzer klickt auf der linken Seite der Seite auf „Abteilung“.
+        Then Der Benutzer uberpruft, ob die Finanzabteilung angezeigt wird
+
+
