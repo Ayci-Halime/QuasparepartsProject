de3e93e724ef183ecc12496659120577d0b79ae9
Sibel Tokgöz <Sibel.M.Tokgoez@gmail.com>
2024-03-20 14:43:24 +0100

    Company neue

--- src/test/java/pages/RolesPage.java
+++ src/test/java/pages/RolesPage.java
@@ -5,7 +5,7 @@ import utilities.ParallelDriver;
 
 public class RolesPage {
     public static void main(String[] args) {
-        System.out.println("RollesPage");
+
     }
 
     public RolesPage() { // bu constructor her page sayfasında standart olacak
