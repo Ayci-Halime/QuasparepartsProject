dab4220bbb30d5eab4512e335860bb8273737e64
Sibel Tokgöz <Sibel.M.Tokgoez@gmail.com>
2024-03-20 11:57:32 +0100

    Company arbeitet

---
+++ src/test/java/stepdefinitions/PermissionSD.java
@@ -0,0 +1,19 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class PermissionSD {
+    @Given("Benutzer klickt auf der linken Seite der Seite auf „ Permissions“.")
+    public void benutzerKlicktAufDerLinkenSeiteDerSeiteAufPermissions() {
+    }
+
+    @And("Der Benutzer klickt links auf der Seite auf Berechtigungen.")
+    public void derBenutzerKlicktLinksAufDerSeiteAufBerechtigungen() {
+    }
+
+    @Then("Der Benutzer sieht, dass der accounting_read angekommen ist.")
+    public void derBenutzerSiehtDassDerAccounting_readAngekommenIst() {
+    }
+}
