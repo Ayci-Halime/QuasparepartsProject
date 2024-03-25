52c0ecaf468e3965cfc510580eb700e4824150fc
Ayci-Halime <135063487+Ayci-Halime@users.noreply.github.com>
2024-03-25 15:36:54 +0100

    Update RunnerSmoke.java

--- src/test/java/runners/RunnerSmoke.java
+++ src/test/java/runners/RunnerSmoke.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
 
         plugin = {
                 "pretty",
-                "html:target/default-cucumber-reports",
-                "json:target/cucumber.json",
+//                "html:target/default-cucumber-reports",
+//                "json:target/cucumber.json",
                 "rerun:target/failedRerun.txt",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
